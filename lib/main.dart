2a422ee6b715a3f49757ebf3835061e8dd9f3274
Saul Mestanza <saul.mm92@gmail.com>
2024-11-17 13:50:01 -0500

    Update main with todo provider and check login status

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'provider/login_provider.dart';
+import 'provider/todo_provider.dart';
 import 'screens/home.dart';
 import 'screens/login.dart';
 
@@ -17,12 +18,19 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Consumer<LoginProvider>(
-        builder: (context, auth, _) {
-          return auth.isLoggedIn ? HomeScreen() : LoginScreen();
-        },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LoginProvider()),
+        ChangeNotifierProvider(create: (context) => TodoProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Consumer<LoginProvider>(
+          builder: (context, auth, _) {
+            auth.checkLoginStatus();
+            return auth.isLoggedIn ? HomeScreen() : LoginScreen();
+          },
+        ),
       ),
     );
   }
